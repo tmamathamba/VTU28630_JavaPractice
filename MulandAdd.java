a8b9c0665e86f170a128ad17cc930cc4f2d421d2
tmamathamba <vtu28630@veltech.edu.in>
2026-09-26 22:17:05 +0530

    Add files via upload

---
+++ MulandAdd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class MulandAdd{
+    public static void main(String [] args){
+        Scanner sc= new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int d=a*b*c;
+        int e=a+b+c;
+        int f=d/e;
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
